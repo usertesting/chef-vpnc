e340e08ec3a7ce17fe5af22890bbde941fcd4086
troyready <troy@troyready.com>
2015-03-25 17:17:31 -0700

    add source/issues links to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,7 @@ license          'apache2'
 description      'Installs/Configures vpnc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+source_url       'https://github.com/troyready/chef-vpnc'
+issues_url       'https://github.com/troyready/chef-vpnc/issues'
 
 supports         'ubuntu'
