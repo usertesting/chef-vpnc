1502399fa1827e1e28a480eb4eb0c4fd25995592
CJ Bryan and Doc Ritezel <pair+cj+doc@usertesting.com>
2015-09-28 14:10:37 -0700

    Remove Chef 11-unsupported source_url and issues_url from metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,6 @@ license          'apache2'
 description      'Installs/Configures vpnc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
-source_url       'https://github.com/troyready/chef-vpnc'
-issues_url       'https://github.com/troyready/chef-vpnc/issues'
 
 supports         'amazon'
 supports         'centos'
